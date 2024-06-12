5a0329904494f1cbb188a7487a2a730d16a35b84
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-11 19:03:10 -0700

    Commit

---
+++ Threading/SecondCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SecondCollectionViewCell.swift
+//  Threading
+//
+//  Created by Arpit iOS Dev. on 11/06/24.
+//
+
+import UIKit
+
+class SecondCollectionViewCell: UICollectionViewCell {
+    
+}
